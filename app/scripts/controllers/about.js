d8721776032288e4b3909b0c7c80f6922f1669ab
Dov DeKeyser <dovdekeyser@yahoo.com>
2017-05-26 12:25:32 -0700

    added angular and updated grunt.js file

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shelterFinder3App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shelterFinder3App
+ */
+angular.module('shelterFinder3App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shelterFinder3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shelterFinder3App
+ */
+angular.module('shelterFinder3App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
