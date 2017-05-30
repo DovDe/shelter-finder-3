4f70a890f40db478be5947358313ffade2275ea9
Dov DeKeyser <dovdekeyser@yahoo.com>
2017-05-30 13:06:12 -0700

    added cross origins

--- app/scripts/services/current.js
+++ app/scripts/services/current.js
@@ -13,12 +13,12 @@ angular.module('shelterFinder3App')
 
 
     // Public API here
-     return $resource('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=:lat,:lng&radius=500&keyword=homeless+shelter&key=AIzaSyD7YjbLtUlWB6hn-VeTBJEhdch7sCOJVO0', {}, {
+     return $resource('https://crossorigin.me/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=:lat,:lng&radius=500&keyword=homeless+shelter&key=AIzaSyD7YjbLtUlWB6hn-VeTBJEhdch7sCOJVO0', {}, {
        query: {
          method:'GET',
          params:{
             lat:'47.6062095',
-            lng: '-122.3320708'
+            lng:'-122.3320708'
          },
          isArray:false
        }
